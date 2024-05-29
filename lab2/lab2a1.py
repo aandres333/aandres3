1943d6a3d12c8ba2128b991db9724886358df315
Alyx Andres <alyxmillan20@gmail.com>
2024-05-29 19:40:59 -0400

    Added lab2a1.py

---
+++ lab2/lab2a1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Name: Alyx Millan Andres
+Course: Ops445NCC
+'''
+name = 'Jon'
+age = 20
+#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+colour = input("Type in a colour and press enter: ")
+print('The colour I typed in is: ' + colour)
