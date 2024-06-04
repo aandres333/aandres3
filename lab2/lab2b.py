a7e0423082db9fae4f51c018c042dbc6170c186c
Alyx Andres <alyxmillan20@gmail.com>
2024-06-04 18:54:09 -0400

    Added lab

---
+++ lab2/lab2b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+Name: Alyx Millan Andres
+Course: Ops445NCC
+'''
+name = input('Name: ')
+age = input('Age: ')
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
---
+++ lab2/lab2c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Name: Alyx Millan Andres
+Course: Ops445NCC
+'''
+import sys
+
+name = sys.argv[1]
+age = sys.argv[2]
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
