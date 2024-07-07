a89637910a8381314cb217a9d1fc0dcfe3ab5614
Alyx Andres <alyxmillan20@gmail.com>
2024-07-07 13:59:31 -0400

    Added lab

---
+++ lab4/lab4e.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Author ID: 106879224
+
+def is_digits(sobj):
+    # Check if all characters in the string are digits
+    for char in sobj:
+        if not char.isdigit():
+            return False
+    return True
+
+if __name__ == '__main__':
+    test_list = ['x3058','3058','8503x','8503']
+    for item in test_list:
+        if is_digits(item):
+            print(item, 'is an integer.')
+        else:
+            print(item, 'is not an integer.')
+
