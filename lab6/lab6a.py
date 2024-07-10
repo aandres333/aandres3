9faa5c6736de2406c13c28ac0fe194e3df6b1f82
Alyx Andres <alyxmillan20@gmail.com>
2024-07-10 14:11:57 -0400

    Added lab

---
+++ lab6/lab6a.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Author ID: 106879224, Alyx Millan Andres
+
+class Student:
+    # Define the name and number when a student object is created, ex. student1 = Student('john', 025969102)
+    def __init__(self, name, number):
+        self.name = name
+        self.number = str(number)  # Ensure number is a string
+        self.courses = {}
+
+    # Return student name and number
+    def displayStudent(self):
+        return 'Student Name: ' + self.name + '\n' + 'Student Number: ' + self.number
+
+    # Add a new course and grade to students record
+    def addGrade(self, course, grade):
+        self.courses[course] = grade
+
+    # Calculate the grade point average of all courses and return a string
+    def displayGPA(self):
+        gpa = 0.0
+        num_courses = len(self.courses)
+        if num_courses == 0:  # Handle the ZeroDivisionError
+            return 'GPA of student ' + self.name + ' is N/A (no courses taken)'
+        for course in self.courses.keys():
+            gpa = gpa + self.courses[course]
+        return 'GPA of student ' + self.name + ' is ' + str(gpa / num_courses)
+
+    # Return a list of courses that the student passed (not a 0.0 grade)
+    def displayCourses(self):
+        passed_courses = [course for course, grade in self.courses.items() if grade > 0.0]
+        return passed_courses
+
+if __name__ == '__main__':
+    # Create first student object and add grades for each class
+    student1 = Student('John', '013454900')
+    student1.addGrade('uli101', 1.0)
+    student1.addGrade('ops245', 2.0)
+    student1.addGrade('ops445', 3.0)
+
+    # Create second student object and add grades for each class
+    student2 = Student('Jessica', 123456)  # Note the integer ID here
+    student2.addGrade('ipc144', 4.0)
+    student2.addGrade('cpp244', 3.5)
+    student2.addGrade('cpp344', 0.0)
+
+    # Display information for student1 object
+    print(student1.displayStudent())
+    print(student1.displayGPA())
+    print(student1.displayCourses())
+
+    # Display information for student2 object
+    print(student2.displayStudent())
+    print(student2.displayGPA())
+    print(student2.displayCourses())
+
